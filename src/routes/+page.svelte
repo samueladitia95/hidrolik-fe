b7ed7ea00bb562357fdc1146c62ffdad6a2a9d59
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-07-27 23:29:31 +0700

    feat: add trusted partner container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import BannerContainer from './containers/BannerContainer.svelte';
 	import TrustedIndustryContainer from './containers/TrustedIndustryContainer.svelte';
+	import TrustedPartnerContainer from './containers/TrustedPartnerContainer.svelte';
 </script>
 
 <div>
 	<BannerContainer />
 	<TrustedIndustryContainer />
+	<TrustedPartnerContainer />
 </div>
---
+++ src/routes/containers/TrustedPartnerContainer.svelte
@@ -0,0 +1,28 @@
+<div class="bg-white">
+	<div class="container py-28">
+		<div class="flex flex-col gap-6 items-start md:items-center md:text-center">
+			<div class="text-3xl/normal md:text-5xl/normal font-bold lg:max-w-4xl">
+				Your Trusted Partner for Premium Hydraulic Hoses
+			</div>
+			<div class="leading-relaxed lg:text-lg/normal">
+				Dedicated to providing top-tier hydraulic hoses across a multitude of industries and
+				applications. Our commitment to excellence ensures that you receive the best quality
+				products at the most competitive prices available.
+			</div>
+			<button class="px-6 py-3 rounded-full bg-black text-white font-semibold">Learn More</button>
+		</div>
+
+		<div class="mt-10 flex justify-center">
+			<iframe
+				width="100%"
+				src="https://www.youtube.com/embed/aA2IRoPFIn0?si=KPHOB2tCdrUnmOC8"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+				class="rounded-lg h-56 md:h-[408px] lg:h-[487px] lg:w-[903px]"
+			></iframe>
+		</div>
+	</div>
+</div>
