9d4dabfc9ab22e8e7a4efd19f1113f0843e83034
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-07-27 13:32:07 +0700

    feat: add modal heeader

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import menuIconBlack from '$lib/icons/menu-icon-black.svg';
 	import mainLogoBlack from '$lib/icons/main-logo-black.svg';
+	import HeaderModal from './HeaderModal.svelte';
+
+	let isModalOpen = false;
 	const navbarItems = [
 		{
 			label: 'About Us'
@@ -24,9 +27,9 @@
 			<img src={mainLogoBlack} alt="main-logo" class="max-w-56" />
 		</div>
 
-		<div>
+		<button on:click={() => (isModalOpen = true)}>
 			<img src={menuIconBlack} alt="icon" class="h-6 w-6" />
-		</div>
+		</button>
 
 		<div class="hidden">
 			{#each navbarItems as item}
@@ -37,3 +40,7 @@
 		</div>
 	</div>
 </div>
+
+{#if isModalOpen}
+	<HeaderModal bind:isModalOpen />
+{/if}
