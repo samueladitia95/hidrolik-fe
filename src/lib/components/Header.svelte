7639c04a8eae422bf46539d4bd9ddea018fce741
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-07-27 13:23:42 +0700

    feat: add top header to layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import menuIconBlack from '$lib/icons/menu-icon-black.svg';
+	import mainLogoBlack from '$lib/icons/main-logo-black.svg';
+	const navbarItems = [
+		{
+			label: 'About Us'
+		},
+		{
+			label: 'Service'
+		},
+		{
+			label: 'Products'
+		},
+		{
+			label: 'Contact Us'
+		}
+	];
+</script>
+
+<div class="w-full">
+	<div class="container py-8 flex justify-between items-start">
+		<!-- Main Logo -->
+		<div>
+			<img src={mainLogoBlack} alt="main-logo" class="max-w-56" />
+		</div>
+
+		<div>
+			<img src={menuIconBlack} alt="icon" class="h-6 w-6" />
+		</div>
+
+		<div class="hidden">
+			{#each navbarItems as item}
+				<div>
+					<div>{item.label}</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+<div>
+	<Header />
+	<slot />
+</div>
