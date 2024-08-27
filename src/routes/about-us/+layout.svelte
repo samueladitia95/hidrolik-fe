e57047600cdfa5613e33fe2b1dfcea46c831f8a4
Samuel Aditia <samueladitia95@gmail.com>
2024-08-27 20:45:52 +0700

    update

--- src/routes/about-us/+layout.svelte
+++ src/routes/about-us/+layout.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import { isTopbarTransparent } from '$lib/store';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		isTopbarTransparent.set(false);
+	});
+</script>
+
 <div class="pt-24 md:!pt-36">
 	<slot />
 </div>
