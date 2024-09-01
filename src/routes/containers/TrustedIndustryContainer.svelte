45902491d71ca1f6460e19b13718cb3ff2241971
Samuel Aditia <samueladitia95@gmail.com>
2024-09-01 23:18:51 +0700

    update

--- src/routes/containers/TrustedIndustryContainer.svelte
+++ src/routes/containers/TrustedIndustryContainer.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { pb } from '$lib/pocketbase';
+	import { inview } from 'svelte-inview';
 	import type { PageData } from '../$types';
+	import { fade, fly } from 'svelte/transition';
 
 	let data = $page.data as PageData;
 	let industries = data.trustedIndustries.map((element) => {
@@ -9,16 +11,39 @@
 			image: pb.files.getUrl(element, element.image_url, { thumb: '1920x1080' })
 		};
 	});
+
+	let isShow: boolean = false;
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
-<div class="bg-white py-20">
+<div
+	use:inview={{
+		rootMargin: '-100px'
+	}}
+	on:inview_change={handleChange}
+	class="bg-white py-20"
+>
 	<div class="container flex flex-col gap-6">
-		<div class="font-bold text-center text-base/relaxed">
-			Trusted by industry leaders for superior quality and reliability
-		</div>
+		{#if isShow}
+			<div
+				class="font-bold text-center text-base/relaxed"
+				transition:fade={{ duration: 500, delay: 1000 }}
+			>
+				Trusted by industry leaders for superior quality and reliability
+			</div>
+		{/if}
 		<div class="flex flex-wrap justify-center gap-6">
-			{#each industries as industry}
-				<img src={industry.image} alt="industry" class="w-36 h-14" />
+			{#each industries as industry, index}
+				{#if isShow}
+					<img
+						src={industry.image}
+						alt="industry"
+						class="w-36 h-14"
+						transition:fly={{ x: 200, duration: 1000 + 100 * index, delay: 500 }}
+					/>
+				{/if}
 			{/each}
 		</div>
 	</div>
