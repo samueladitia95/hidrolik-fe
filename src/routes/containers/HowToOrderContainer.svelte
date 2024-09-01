0de0507fa0995b1fe41ec3bdfc3f0de2f98c5f56
Samuel Aditia <samueladitia95@gmail.com>
2024-09-01 23:42:47 +0700

    update

--- src/routes/containers/HowToOrderContainer.svelte
+++ src/routes/containers/HowToOrderContainer.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import boxLogo from '$lib/icons/box-logo.svg?raw';
+	import { inview } from 'svelte-inview';
+	import { fade, fly } from 'svelte/transition';
 	const howToOrders = [
 		{
 			title: 'Browse our products',
@@ -26,28 +28,49 @@
 			isChatButton: false
 		}
 	];
+
+	let isShow: boolean = false;
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
-<div class="bg-secondary w-screen">
+<div
+	class="bg-secondary w-screen min-h-screen"
+	use:inview={{
+		rootMargin: '-100px'
+	}}
+	on:inview_change={handleChange}
+>
 	<div class="container py-28 md:!py-56 text-white">
 		<div class="xl:!flex xl:!items-center xl:!gap-40">
-			<div class="flex flex-col gap-6 text-left max-w-[500px] xl:!max-w-[1000px] xl:!items-center xl:!w-[1000px]">
-				<div class="text-3xl md:!text-5xl/relaxed font-bold">How to order our products</div>
-			</div>
+			{#if isShow}
+				<div
+					class="flex flex-col gap-6 text-left max-w-[500px] xl:!max-w-[1000px] xl:!items-center xl:!w-[1000px]"
+					transition:fade={{ duration: 500, delay: 500 }}
+				>
+					<div class="text-3xl md:!text-5xl/relaxed font-bold">How to order our products</div>
+				</div>
+			{/if}
 
 			<div class="mt-20 grid grid-cols-1 md:!grid-cols-2 gap-12 md:!gap-24">
 				{#each howToOrders as order, index}
-					<div class="flex flex-col gap-6 items-start">
-						<div class="w-12 h-12">{@html boxLogo}</div>
-						<div class="text-2xl font-bold">{index + 1}. {order.title}</div>
-						<div class="text-sm/loose">{order.subTitle}</div>
-						{#if order.isChatButton}
-							<button
-								class="px-6 py-3 rounded-full bg-black text-white font-semibold hover:bg-opacity-75"
-								>Chat Via WhatsApp</button
-							>
-						{/if}
-					</div>
+					{#if isShow}
+						<div
+							class="flex flex-col gap-6 items-start"
+							transition:fly={{ x: -200, duration: 500 + 100 * index, delay: 500 }}
+						>
+							<div class="w-12 h-12">{@html boxLogo}</div>
+							<div class="text-2xl font-bold">{index + 1}. {order.title}</div>
+							<div class="text-sm/loose">{order.subTitle}</div>
+							{#if order.isChatButton}
+								<button
+									class="px-6 py-3 rounded-full bg-black text-white font-semibold hover:bg-opacity-75"
+									>Chat Via WhatsApp</button
+								>
+							{/if}
+						</div>
+					{/if}
 				{/each}
 			</div>
 		</div>
