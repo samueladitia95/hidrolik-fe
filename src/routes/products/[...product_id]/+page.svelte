ba8832287b13031259aea53888b85cf1b3efe189
Samuel Aditia <samueladitia95@gmail.com>
2024-09-02 00:07:32 +0700

    update

--- src/routes/products/[...product_id]/+page.svelte
+++ src/routes/products/[...product_id]/+page.svelte
@@ -6,6 +6,8 @@
 
 	import type { PageData } from './$types';
 	import ContactUsModal from '$lib/components/ContactUsModal.svelte';
+	import { inview } from 'svelte-inview';
+	import { fade } from 'svelte/transition';
 	export let data: PageData;
 
 	$: images = data.product.images_url.map((element: string) => {
@@ -20,79 +22,101 @@
 	}
 
 	let showModal: boolean = false;
+
+	let isShow: boolean = false;
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
-<div class="container">
+<div
+	class="container min-h-screen"
+	use:inview={{
+		rootMargin: '-100px'
+	}}
+	on:inview_change={handleChange}
+>
 	<!-- BREADCRUMB -->
-	<div class="text-xs flex items-center gap-1">
-		<div>Product</div>
-		<div class="h-4 w-4 -rotate-90">
-			{@html ChevronLogo}
+	{#if isShow}
+		<div class="text-xs flex items-center gap-1" transition:fade={{ duration: 500, delay: 0 }}>
+			<div>Product</div>
+			<div class="h-4 w-4 -rotate-90">
+				{@html ChevronLogo}
+			</div>
+			<div>{data.parent_categories}</div>
+			<div class="h-4 w-4 -rotate-90">
+				{@html ChevronLogo}
+			</div>
+			<div class="font-semibold">{data.product.name}</div>
 		</div>
-		<div>{data.parent_categories}</div>
-		<div class="h-4 w-4 -rotate-90">
-			{@html ChevronLogo}
-		</div>
-		<div class="font-semibold">{data.product.name}</div>
-	</div>
+	{/if}
 
 	<!-- PRODUCT NAME AND DESCRIPTION -->
-	<div class="mt-6 flex flex-col gap-3">
-		<div class="flex gap-4 items-start">
-			<button class="w-10 h-10 min-h-10 min-w-10" on:click={() => window.history.back()}>
-				{@html BackArrowLogo}
-			</button>
-			<div class="text-3xl font-bold">
-				{data.product.name}
-			</div>
-		</div>
-		<div class="font-bold">
-			{data.product.description}
-		</div>
-	</div>
-
-	<div class="lg:!grid lg:!grid-cols-2 lg:!gap-6 lg:!mt-6">
-		<!-- GALLERY -->
-		<div class="py-6 lg:!pt-0 flex flex-col gap-2">
-			<div bind:this={containerEl} class="flex flex-start overflow-hidden snap-x snap-mandatory">
-				{#each images as src}
-					<img {src} alt="product" class="max-w-full rounded snap-center" />
-				{/each}
-			</div>
-			<div class="w-full grid grid-cols-5 gap-2">
-				{#each images as src, index}
-					<button on:click={() => scrollIntoView(index, images.length)}>
-						<img {src} alt="product" class="rounded object-cover w-full h-full" />
-					</button>
-				{/each}
-			</div>
-		</div>
-		<div>
-			<!-- DETAIL -->
-			<div class="py-2 lg:!pt-0">
-				<div class="flex border-b border-solid border-black border-opacity-25">
-					<div class="p-2 font-semibold border-b-2 border-solid border-secondary">Detail</div>
-				</div>
-
-				<div class="py-6">
-					{@html data.product.detail}
+	{#if isShow}
+		<div class="mt-6 flex flex-col gap-3" transition:fade={{ duration: 500, delay: 100 }}>
+			<div class="flex gap-4 items-start">
+				<button class="w-10 h-10 min-h-10 min-w-10" on:click={() => window.history.back()}>
+					{@html BackArrowLogo}
+				</button>
+				<div class="text-3xl font-bold">
+					{data.product.name}
 				</div>
 			</div>
+			<div class="font-bold">
+				{data.product.description}
+			</div>
+		</div>
+	{/if}
 
-			<!-- INQUIRY BUTTON -->
+	{#if isShow}
+		<div
+			class="lg:!grid lg:!grid-cols-2 lg:!gap-6 lg:!mt-6"
+			transition:fade={{ duration: 500, delay: 200 }}
+		>
+			<!-- GALLERY -->
+			<div class="py-6 lg:!pt-0 flex flex-col gap-2">
+				<div bind:this={containerEl} class="flex flex-start overflow-hidden snap-x snap-mandatory">
+					{#each images as src}
+						<img {src} alt="product" class="max-w-full rounded snap-center" />
+					{/each}
+				</div>
+				<div class="w-full grid grid-cols-5 gap-2">
+					{#each images as src, index}
+						<button on:click={() => scrollIntoView(index, images.length)}>
+							<img {src} alt="product" class="rounded object-cover w-full h-full" />
+						</button>
+					{/each}
+				</div>
+			</div>
 			<div>
-				<button
-					class="px-6 py-3 font-semibold rounded-full bg-black text-white hover:bg-opacity-75"
-					on:click={() => (showModal = true)}>Inquiry Now</button
-				>
+				<!-- DETAIL -->
+				<div class="py-2 lg:!pt-0">
+					<div class="flex border-b border-solid border-black border-opacity-25">
+						<div class="p-2 font-semibold border-b-2 border-solid border-secondary">Detail</div>
+					</div>
+
+					<div class="py-6">
+						{@html data.product.detail}
+					</div>
+				</div>
+
+				<!-- INQUIRY BUTTON -->
+				<div>
+					<button
+						class="px-6 py-3 font-semibold rounded-full bg-black text-white hover:bg-opacity-75"
+						on:click={() => (showModal = true)}>Inquiry Now</button
+					>
+				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 
 	<!-- RECOMMENDATION -->
-	<div class="py-20">
-		<div class="text-2xl font-bold">You might also like</div>
-	</div>
+	{#if isShow}
+		<div class="py-20" transition:fade={{ duration: 500, delay: 300 }}>
+			<div class="text-2xl font-bold">You might also like</div>
+		</div>
+	{/if}
 </div>
 
 <ContactUsModal bind:showModal />
