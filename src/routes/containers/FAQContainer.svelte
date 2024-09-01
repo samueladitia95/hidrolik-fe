6f35301803d5747cacbeb3e10aa59d39dfeb10f4
Samuel Aditia <samueladitia95@gmail.com>
2024-09-01 23:51:24 +0700

    update

--- src/routes/containers/FAQContainer.svelte
+++ src/routes/containers/FAQContainer.svelte
@@ -4,6 +4,8 @@
 
 	import { page } from '$app/stores';
 	import type { PageData } from '../$types';
+	import { inview } from 'svelte-inview';
+	import { fade, fly } from 'svelte/transition';
 
 	let data = $page.data as PageData;
 	let faqs = data.faqs.map((element, index) => {
@@ -24,44 +26,60 @@
 			return faq;
 		});
 	};
+
+	let isShow: boolean = false;
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
 <div
 	class="bg-gradient-to-r from-[#05374F] to-[#08222F] text-white min-h-screen lg:!flex lg:!flex-col lg:!justify-center"
+	use:inview={{
+		rootMargin: '-100px'
+	}}
+	on:inview_change={handleChange}
 >
-	<div class="container py-28 lg:py-40">
-		<div
-			class="text-3xl/relaxed md:!text-5xl/relaxed font-bold text-left md:!text-center w-full flex justify-center"
-		>
-			<div class="max-w-xl">Frequently Asked Questions</div>
-		</div>
+	<div class="container py-28 lg:py-40" transition:fade={{ duration: 500, delay: 500 }}>
+		{#if isShow}
+			<div
+				class="text-3xl/relaxed md:!text-5xl/relaxed font-bold text-left md:!text-center w-full flex justify-center"
+			>
+				<div class="max-w-xl">Frequently Asked Questions</div>
+			</div>
+		{/if}
 		<div class="mt-16 flex flex-col gap-12">
 			{#each faqs as faq, index}
-				<div class="flex flex-col gap-6">
-					<div class="flex items-center justify-between">
-						<div class="text-xl/relaxed md:!text-2xl/relaxed font-bold">{faq.question}</div>
-						<button
-							class={clsx(
-								'w-12 h-12',
-								'transition-transform duration-200',
-								faq.isOpen ? 'rotate-180' : ' rotate-0'
-							)}
-							on:click={() => handleOpenAccordion(index, !faq.isOpen)}
-						>
-							{@html chevronLogo}
-						</button>
-					</div>
+				{#if isShow}
 					<div
-						class={clsx(
-							'text-sm/relaxed md:!text-base/relaxed',
-							'transition-all duration-400 overflow-hidden',
-							faq.isOpen ? 'max-h-[200px]' : 'max-h-0'
-						)}
+						class="flex flex-col gap-6"
+						transition:fly={{ x: 200, duration: 500 + 100 * index, delay: 500 }}
 					>
-						{faq.answers}
+						<div class="flex items-center justify-between">
+							<div class="text-xl/relaxed md:!text-2xl/relaxed font-bold">{faq.question}</div>
+							<button
+								class={clsx(
+									'w-12 h-12',
+									'transition-transform duration-200',
+									faq.isOpen ? 'rotate-180' : ' rotate-0'
+								)}
+								on:click={() => handleOpenAccordion(index, !faq.isOpen)}
+							>
+								{@html chevronLogo}
+							</button>
+						</div>
+						<div
+							class={clsx(
+								'text-sm/relaxed md:!text-base/relaxed',
+								'transition-all duration-400 overflow-hidden',
+								faq.isOpen ? 'max-h-[200px]' : 'max-h-0'
+							)}
+						>
+							{faq.answers}
+						</div>
+						<div class="border-t border-solid border-white" />
 					</div>
-					<div class="border-t border-solid border-white" />
-				</div>
+				{/if}
 			{/each}
 		</div>
 	</div>
