dc48be0f51a070f78817b0e16d8d43f788ab537f
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-09-01 20:54:37 +0700

    update

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -221,7 +221,7 @@
 
 			<div class="mt-6">
 				{#if products.length}
-					<div class="grid grid-cols-1 md:!grid-cols-2 lg:!grid-cols-3 gap-16">
+					<div class="grid grid-cols-1 md:!grid-cols-2 lg:!grid-cols-3 gap-y-16 gap-x-6">
 						{#each products as product, index}
 							<a href={`/products/${product.id}`}>
 								<div class="flex flex-col gap-4">
